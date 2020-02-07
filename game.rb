63e05cb877bacac8083097af19a64bc04dd239eb
Stuart Myers <stuart.myers@gmail.com>
2020-02-07 17:48:56 +0800

    added global variable, I think it's a bad idea

--- game.rb
+++ game.rb
@@ -29,46 +29,81 @@ class Monster
   end
 end
 
+# The monster truck carries monsters
+class MonsterTruck < Monster
+
+  def initialize(monster_array, name)
+    super(name)
+    @monster_array = monster_array
+    @hp = 20
+  end
+
+  def how_many_inside
+    @monster_array.length
+  end
+
+  def set_hp(damage)
+    @hp = @hp - damage
+    if @hp <= 0
+      destroy_truck
+    end
+  end
+
+  def on_board_roar
+    @monster_array.each do |monster|
+      monster.roar
+    end
+  end
+
+  def destroy_truck
+    puts "the monster truck has been destroyed!"
+    puts "The other monsters come out of the truck and start fighting!"
+    @monster_array.each do |monster|
+      $monster_array << monster
+    end
+  end
+
+end
+
 class Player
-end
-#
-# stuart = Monster.new("Stuart")
-# stuart.roar()
-# stuart.put_hp()
-#
-# eunice = Monster.new("Eunice")
-# eunice.roar()
-# eunice.put_hp()
-#
-# puts "Attack stuart for 3 damage"
-# stuart.set_hp(3)
-# stuart.put_hp()
-# eunice.put_hp()
 
-monsterArray = []
-
-puts "How many monsters would you like to fight?"
-input_number = gets.chomp.to_i
-for i in 1..input_number do
-  monsterArray << Monster.new
 end
 
-while monsterArray.length > 0 do
-  monsterArray.each_with_index do |monster, index|
+
+$monster_array = [MonsterTruck.new([Monster.new, Monster.new, Monster.new], "Monster Truck")]
+
+# puts "How many monsters would you like to fight?"
+# input_number = gets.chomp.to_i
+# for i in 1..input_number do
+#   $monster_array << Monster.new
+# end
+
+puts "The Monster truck approaches"
+$monster_array[0].on_board_roar
+
+while $monster_array.length > 0 do
+  $monster_array.each_with_index do |monster, index|
     puts "Monster index: " + index.to_s
     monster.put_hp
   end
+
   puts "which monster would you like to attack?"
   index = gets.chomp.to_i
   puts "how much damage do you want to do?"
   input = gets.chomp.to_i
-  monsterArray[index].set_hp input
-  monsterArray.each_with_index do |monster, index|
+
+  if index > $monster_array.length - 1
+    next
+  end
+
+  $monster_array[index].set_hp input
+  $monster_array.each_with_index do |monster, index|
     if monster.get_hp <= 0
       puts monster.get_name + " has been defeated!"
-      monsterArray.delete_at index
+      $monster_array.delete_at index
     end
   end
+
 end
 
 puts "done with game!"
