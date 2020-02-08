6cba73bfe26f0cc07243811cdf6b729c8c3bef58
SamTanFL <samsite_tan@hotmail.com>
2020-02-08 22:42:15 +0800

    added monster truck but not implemented yet

--- start.rb
+++ start.rb
@@ -1,8 +1,3 @@
-NAMES = [
-    ["Dark", "Red", "Green", "Blue", "White", "Black", "Nasty", "Smelly", "Ugly", "Lonely", "Wretched", "Ruby", "Sickly", "Stubborn", "Unending"],
-    ["Kobold", "Bandit", "Ogre", "Goblin", "Slime", "Golem", "Pony", "Troll", "Orc", "Error", "Syntax"]
-]
-
 MONSTERS = []
 GRAVEYARD = []
 
@@ -19,6 +14,18 @@ class Monster
 
 end
 
+class Monster_Truck
+
+    attr_accessor :name, :hp, :mobs
+
+    def initialize (name, hp, mobs)
+        @name = name
+        @hp = hp
+        @mobs = mobs
+    end
+
+end
+
 class Player
     attr_accessor :hp, :name
 
@@ -27,31 +34,40 @@ class Player
         @name = name
     end
 
-
 end
-
-def generate_name (prefix, base)
-    return prefix + " " + base
-end
-
-
-
 
 
 def create_mob ( number )
-    for i in 1..number do
-        MONSTERS.push( monster = Monster.new( generate_name(NAMES[0][rand(NAMES[0].length)], NAMES[1][rand(NAMES[1].length)] ), rand(25)+55, rand ))
+
+    names = [
+    ["Dark", "Red", "Green", "Blue", "White", "Black", "Nasty", "Smelly", "Ugly", "Lonely", "Wretched", "Ruby", "Sickly", "Stubborn", "Unending"],
+    ["Kobold", "Bandit", "Ogre", "Goblin", "Slime", "Golem", "Pony", "Troll", "Orc", "Error", "Syntax"]
+]
+    def generate_name (prefix, base)
+        return prefix + " " + base
     end
-    puts MONSTERS
+
+    for i in 1..number do
+        MONSTERS.push( monster = Monster.new( generate_name(names[0][rand(names[0].length)], names[1][rand(names[1].length)] ), rand(55)+25, rand ))
+    end
+
 end
 
 
 
 
 puts "Type number of monsters you wish to generate!"
-input = gets.chomp
-p input
-create_mob input.to_i
+mob_count = gets.chomp
+create_mob mob_count.to_i
+prefix = ["Monster", "Raggedy", "Nascar", "Hot Wheels", "Slightly-Used", "Bulletproof", "Squeaky", "Rusty", "Ninja" "Clown"]
+truck_name = prefix[rand(prefix.length)] + " Monster Truck"
+truck = Monster_Truck.new( truck_name, rand(55)+25, MONSTERS )
+
+
+puts truck.name
+puts truck.hp
+puts truck.mobs
+
 
 
 while MONSTERS.length > 0 do
@@ -60,8 +76,8 @@ while MONSTERS.length > 0 do
         puts "Health : #{MONSTERS[0].hp}"
         puts "how much damage do you want to do?"
         input = gets.chomp
+        puts "You deal " + input + " damage"
         MONSTERS[0].hp -= input.to_i
-        puts "monster has damage: "+MONSTERS[0].hp.to_s
     end
     GRAVEYARD.push MONSTERS.shift
 end
