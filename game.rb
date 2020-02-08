9457dc8f1962852f0b4e0f4aea667d9c4bf7424c
benjacoblee <benjacoblee@gmail.com>
2020-02-08 09:09:03 +0800

    made monster truck, need to set up condition to attack monster truck before attacking monsters

--- game.rb
+++ game.rb
@@ -12,30 +12,52 @@ class Monster
   end
 end
 
+class Truck
+  def initialize (hp)
+    @hp = hp
+    @monsters = []
+  end
+
+  def get_hp
+    @hp
+  end
+
+  def set_hp(damage)
+    @hp = @hp - damage
+  end
+
+  def make_monsters(monster_count) 
+    while @monsters.length < monster_count
+    @monsters.push(Monster.new)
+  end
+
+  def truck_monsters
+    @monsters
+  end
+end
+end
+
 class Player
 end
 
-monster1 = Monster.new
-monster2 = Monster.new
-monster3 = Monster.new
-
-monsters = []
-
+puts "How much HP should the truck have?"
+truckHP = gets.chomp.to_i
 puts "How many monsters should be in the game?"
 monster_count = gets.chomp.to_i
- while monsters.length < monster_count
-  monsters.push(Monster.new)
- end
 
-monsters.each_with_index do |monster, index|
+truck = Truck.new(truckHP)
+
+truck.make_monsters(monster_count)
+
+truck.truck_monsters.each_with_index do |monster, index|
   while monster.get_hp > 0
   puts "Which monster do you wanna attack?"
   chosen_monster = gets.chomp.to_i
   puts "How much damage do you wanna do?"
   damage = gets.chomp
-  monsters[chosen_monster].set_hp damage.to_i
+  truck.truck_monsters[chosen_monster].set_hp damage.to_i
   puts "You did " + damage + " damage to monster " + chosen_monster.to_s
-    monsters.each_with_index do |monster, index|
+    truck.truck_monsters.each_with_index do |monster, index|
     puts "Monster " + index.to_s 
     puts "Remaining hp:" + monster.get_hp.to_s
     end
