74714cc3c040099f52f1a3d991c6ecf20157b893
Robert Kolsek <kolsek.income@gmail.com>
2020-02-09 00:41:49 +0800

    - Auto filled monster array upon starting the game

--- game.rb
+++ game.rb
@@ -23,35 +23,44 @@ monster2 = Monster.new,
 monster3 = Monster.new,
 ]
 
-while monsters.length > 0 do
+monsters = []
 
-  if (monsters.length == 3)
-    puts "Which monster do you want to attack? 1, 2 or 3?"
-    select_input = gets.chomp
-    puts "how much damage do you want to do?"
-    input = gets.chomp
-    monsters[select_input.to_i - 1].set_hp(input.to_i)
-  elsif monsters.length == 2
-    puts "Which monster do you want to attack? 1 or 2?"
-    select_input = gets.chomp
-    puts "how much damage do you want to do?"
-    input = gets.chomp
-    monsters[select_input.to_i - 1].set_hp(input.to_i)
-  else
-    puts "You are fighting one monster!"
-    puts "how much damage do you want to do?"
-    select_input = 1
-    input = gets.chomp
-    monsters[0].set_hp(input.to_i)
+  puts "How many monsters should be in the game?"
+  monster_count = gets.chomp
+  index = 1
+  while index <= monster_count.to_i do
+    monsters.push(monster = Monster.new)
+    index += 1
   end
-  
+
+while monsters.length > 0 do
+  puts "You are fighting #{monsters.length} Monsters"
+
+    if monsters.length > 1
+      puts "Which monster do you want to attack?"
+      select_input = gets.chomp
+      puts "how much damage do you want to do?"
+      input = gets.chomp
+      monsters[select_input.to_i - 1].set_hp(input.to_i)
+    else
+      select_input = 1
+      puts "how much damage do you want to do?"
+      input = gets.chomp
+      monsters[select_input.to_i - 1].set_hp(input.to_i)
+    end
+    
   puts "You, a Paladin with 18 Charisma and 97 hitpoints, attack monster#{select_input} with your Helm of Disintegration, and your Half-elf mage is wielding his +5 holy avenger, causing: "+ input +" Damage"
 # UP NEXT: Check for error, replace the hashtag monster thingy and replace with monster placeholder
+
   monsters.each do |monster|  
-    puts "Remaining HP of #{monster}: #{monster.get_hp}"
     if monster.get_hp == 0
+      puts "You quickly strongly magically heroically enthusiastically pulverized that sucker"
       monsters.delete_at(select_input.to_i - 1)
     end
+  end 
+
+  monsters.each do |monster|  
+    puts "Remaining HP of #{monster}: #{monster.get_hp}"
   end 
 
 end
