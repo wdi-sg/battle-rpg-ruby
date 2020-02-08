4d56de3a80cbf8c0b5eda93f93fb7d5482292e1d
Robert Kolsek <kolsek.income@gmail.com>
2020-02-09 01:19:43 +0800

    - Initialized monsters within truck class

--- game.rb
+++ game.rb
@@ -16,12 +16,12 @@ class Player
 end
 
 class Truck
-  def initialize(monsters)
-    @monsters = monsters
+  def initialize
+    @monsters = []
     @hp = 30
   end
 
-  def get_monsters
+  def monsters
     @monsters
   end
 
@@ -33,25 +33,28 @@ class Truck
     @hp = @hp - damage
   end
 
+  def spawn_monsters(monster_count)
+    index = 1
+    while index <= monster_count.to_i do
+      @monsters.push(monster = Monster.new)
+      index += 1
+    end
+  end
+
 end
 
-
-
-monsters = []
-
-puts "How many monsters should be in the game?"
-monster_count = gets.chomp
-index = 1
-while index <= monster_count.to_i do
-  monsters.push(monster = Monster.new)
-  index += 1
-end
-
-truck = Truck.new(monsters)
-
+# playing around with methods
 def dealDamage(arr, monsterIndex, damage)
   arr[monsterIndex].set_hp(damage)
 end
+# initialize truck
+truck = Truck.new
+# create monster instances
+puts "How many monsters should be in the game?"
+monster_count = gets.chomp
+truck.spawn_monsters(monster_count)
+
+
 
 while truck.get_hp > 0 do
   puts "You are fighting a truck full of monsters! Destroy its armor!"
@@ -66,33 +69,33 @@ while truck.get_hp > 0 do
   end
 end
 
-while monsters.length > 0 && truck.get_hp <= 0 do
-  puts "You are fighting #{monsters.length} Monsters"
+while truck.monsters.length > 0 && truck.get_hp <= 0 do
+  puts "You are fighting #{truck.monsters.length} Monsters"
 
-    if monsters.length > 1
+    if truck.monsters.length > 1
       puts "Which monster do you want to attack?"
       select_input = gets.chomp
       puts "how much damage do you want to do?"
       input = gets.chomp
-      dealDamage(monsters, select_input.to_i - 1, input.to_i)
+      dealDamage(truck.monsters, select_input.to_i - 1, input.to_i)
     else
       select_input = 1
       puts "how much damage do you want to do?"
       input = gets.chomp
-      dealDamage(monsters, select_input.to_i - 1, input.to_i)
+      dealDamage(truck.monsters, select_input.to_i - 1, input.to_i)
     end
 
   puts "You, a Paladin with 18 Charisma and 97 hitpoints, attack monster#{select_input} with your Helm of Disintegration, and your Half-elf mage is wielding his +5 holy avenger, causing: "+ input +" Damage"
 # UP NEXT: Check for error, replace the hashtag monster thingy and replace with monster placeholder
 
-  monsters.each do |monster|  
+  truck.monsters.each do |monster|  
     if monster.get_hp <= 0
       puts "You quickly strongly magically heroically enthusiastically pulverized that sucker"
-      monsters.delete_at(select_input.to_i - 1)
+      truck.monsters.delete_at(select_input.to_i - 1)
     end
   end 
 
-  monsters.each do |monster|  
+  truck.monsters.each do |monster|  
     puts "Remaining HP of #{monster}: #{monster.get_hp}"
   end 
 
