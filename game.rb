448e06f4bb8f728981911a17d254d6528b248303
Robert Kolsek <kolsek.income@gmail.com>
2020-02-09 00:48:51 +0800

    - Further 2 done

--- game.rb
+++ game.rb
@@ -25,13 +25,17 @@ monster3 = Monster.new,
 
 monsters = []
 
-  puts "How many monsters should be in the game?"
-  monster_count = gets.chomp
-  index = 1
-  while index <= monster_count.to_i do
-    monsters.push(monster = Monster.new)
-    index += 1
-  end
+puts "How many monsters should be in the game?"
+monster_count = gets.chomp
+index = 1
+while index <= monster_count.to_i do
+  monsters.push(monster = Monster.new)
+  index += 1
+end
+
+def dealDamage(arr, monsterIndex, damage)
+  arr[monsterIndex].set_hp(damage)
+end
 
 while monsters.length > 0 do
   puts "You are fighting #{monsters.length} Monsters"
@@ -41,19 +45,19 @@ while monsters.length > 0 do
       select_input = gets.chomp
       puts "how much damage do you want to do?"
       input = gets.chomp
-      monsters[select_input.to_i - 1].set_hp(input.to_i)
+      dealDamage(monsters, select_input.to_i - 1, input.to_i)
     else
       select_input = 1
       puts "how much damage do you want to do?"
       input = gets.chomp
-      monsters[select_input.to_i - 1].set_hp(input.to_i)
+      dealDamage(monsters, select_input.to_i - 1, input.to_i)
     end
-    
+
   puts "You, a Paladin with 18 Charisma and 97 hitpoints, attack monster#{select_input} with your Helm of Disintegration, and your Half-elf mage is wielding his +5 holy avenger, causing: "+ input +" Damage"
 # UP NEXT: Check for error, replace the hashtag monster thingy and replace with monster placeholder
 
   monsters.each do |monster|  
-    if monster.get_hp == 0
+    if monster.get_hp <= 0
       puts "You quickly strongly magically heroically enthusiastically pulverized that sucker"
       monsters.delete_at(select_input.to_i - 1)
     end
