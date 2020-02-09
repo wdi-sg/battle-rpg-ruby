4e3279e4b79475d3e7bffca8db6a135cffc77b70
weizheng1910 <weizheng1910@gmail.com>
2020-02-09 11:01:15 +0800

    1

--- game.rb
+++ game.rb
@@ -22,16 +22,29 @@ end
 
 
 
-def check_if_at_least_one_dead(monsters)
-  at_least_one_dead = false
+def check_if_all_are_dead(monsters)
+  all_dead = true
 
   for i in 0...monsters.length do
-    if monsters[i].get_hp < 0 
-      at_least_one_dead = true
+    if monsters[i] != nil
+      all_dead = false
     end 
   end
 
-  return at_least_one_dead
+  return all_dead
+end
+
+def remove_dead_monsters(monsters)
+
+  for i in 0...monsters.length do
+    if monsters[i] == nil
+      next
+    elsif monsters[i].get_hp < 1
+      puts "Monster#{i + 1} is dead!" 
+      monsters[i] = nil
+    end
+  end
+
 end
 
 
@@ -39,16 +52,15 @@ end
 puts "How many monsters do you want to fight"
 monster_count = gets.chomp
 
-
 monsters = Array.new(monster_count.to_i,nil)
 
-for x in 0..monsters.length do
+# .. means up to and equal, ... means not equal 
+for x in 0...monsters.length do
   monsters[x] = Monster.new
 end
 
-while check_if_at_least_one_dead(monsters) == false do
+while check_if_all_are_dead(monsters) == false do
   
-
   puts "Which monster do you want to attack?"
   monster_to_attack = gets.chomp
   monster_to_attack_int = (monster_to_attack.to_i - 1)
@@ -56,15 +68,19 @@ while check_if_at_least_one_dead(monsters) == false do
   puts "how much damage do you want to do?"
   input = gets.chomp
 
-  puts monster_to_attack_int
-  puts monsters[monster_to_attack_int]
-
+  #Subtract HP from chosen monster's health
   monsters[monster_to_attack_int].set_hp input.to_i
 
-  puts "Showing monster HP..."
-  
+  remove_dead_monsters(monsters)
+
+  #Display Monsters HP
+  puts "Showing monster HP..."  
   for c in 0...monsters.length do
-    puts "Monster#{c + 1} HP = #{monsters[c].get_hp}"   
+    if monsters[c] == nil
+      puts "Monster#{c + 1} is dead!"
+    else
+      puts "Monster#{c + 1} HP = #{monsters[c].get_hp}"
+    end  
   end
 
 end
