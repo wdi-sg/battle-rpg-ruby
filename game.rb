f8172dfc1e965fbeec1a15737543315d52b06831
rachellesg <rachelle.yeo@outlook.com>
2020-02-09 19:14:33 +0800

    first commit

---
+++ game.rb
@@ -0,0 +1,39 @@
+class Monster
+  def initialize
+    @hp = 10
+  end
+
+  def get_hp
+    @hp
+  end
+
+  def set_hp(damage)
+    @hp = @hp - damage
+  end
+end
+
+class Player
+end
+
+monster1 = Monster.new
+monster2 = Monster.new
+monster3 = Monster.new
+
+monsters = [monster1, monster2, monster3]
+
+# puts monsters
+
+monsters.each_with_index do |monster, index|
+  puts "which monster do you want to choose? select from 1 - 3"
+  chosen_monster = gets.chomp.to_i
+  puts "you've chosen monster " + chosen_monster.to_s
+  puts monsters[chosen_monster].to_s
+  while monsters[chosen_monster].get_hp > 0
+    puts "how much damage do you want to do?"
+    damage = gets.chomp.to_i
+    monsters[chosen_monster].set_hp damage.to_i
+    puts "monster has damage: " + monsters[chosen_monster].get_hp.to_s
+  end
+end
+
+puts "done with game!"
