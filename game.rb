d1dd8bec525f71f505ac37aa3783bb32e640f6a4
rachellesg <rachelle.yeo@outlook.com>
2020-02-09 19:21:02 +0800

    fixed bug for hp = 0

--- game.rb
+++ game.rb
@@ -23,17 +23,21 @@ monsters = [monster1, monster2, monster3]
 
 # puts monsters
 
-monsters.each_with_index do |monster, index|
-  puts "which monster do you want to choose? select from 1 - 3"
-  chosen_monster = gets.chomp.to_i
-  puts "you've chosen monster " + chosen_monster.to_s
-  puts monsters[chosen_monster].to_s
-  while monsters[chosen_monster].get_hp > 0
-    puts "how much damage do you want to do?"
-    damage = gets.chomp.to_i
-    monsters[chosen_monster].set_hp damage.to_i
-    puts "monster has damage: " + monsters[chosen_monster].get_hp.to_s
+puts "how many monsters should be in the game?"
+monster_count = gets.chomp.to_i
+
+if monster_count > 0
+  monsters.each_with_index do |monster, index|
+    puts "which monster do you want to choose?"
+    chosen_monster = gets.chomp.to_i
+    puts "you've chosen monster " + chosen_monster.to_s
+    puts monsters[chosen_monster].to_s
+    while monsters[chosen_monster].get_hp > 0
+      puts "how much damage do you want to do?"
+      damage = gets.chomp.to_i
+      monsters[chosen_monster].set_hp damage.to_i
+      puts "monster has damage: " + monsters[chosen_monster].get_hp.to_s
+    end
+    puts "done with game!"
   end
 end
-
-puts "done with game!"
